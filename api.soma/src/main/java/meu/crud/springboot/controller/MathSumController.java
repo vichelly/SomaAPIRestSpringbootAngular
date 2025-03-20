6a8bba05a37649358c29df1ec4114132f51348a1
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2025-03-20 18:44:02 -0300

    feat: i hate angularjs v19

--- api.soma/src/main/java/meu/crud/springboot/controller/MathSumController.java
+++ api.soma/src/main/java/meu/crud/springboot/controller/MathSumController.java
@@ -3,6 +3,7 @@ package meu.crud.springboot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @ResponseBody
 @RequestMapping("/math")
+@CrossOrigin("http://localhost:4200")
 public class MathSumController {
     @GetMapping("/sum/{num1}/{num2}")
     public Double getSum(@PathVariable Double num1,@PathVariable Double num2) {
